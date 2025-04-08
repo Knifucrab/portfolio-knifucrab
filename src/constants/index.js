29b49aafe9d261ff6633261d523dc334ed2b720e
Knifucrab <bringasmaurosebastian@gmail.com>
2025-04-08 20:09:23 -0300

    updated experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,32 +19,32 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: '2023 - Present',
-    role: 'Senior Full Stack Developer',
-    company: 'Google Inc.',
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ['Javascript', 'React.js', 'Next.js', 'mongoDB'],
+    year: 'March 2024 - August 2024 (6 months)',
+    role: 'Front End Developer',
+    company: 'Petsion (Final University Project)',
+    description: `Collaborated in a team project as part of my final university coursework, with professors from multinational companies acting as real-world clients.`,
+    technologies: [
+      'React',
+      'Bootstrap',
+      'Redux',
+      'mongoDB',
+      'Express',
+      'SCRUM',
+      'REST API',
+    ],
   },
   {
-    year: '2022 - 2023',
-    role: 'Frontend Developer',
-    company: 'Adobe',
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ['HTML', 'CSS', 'Vue.js', 'mySQL'],
-  },
-  {
-    year: '2021 - 2022',
-    role: 'Full Stack Developer',
-    company: 'Facebook',
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ['Python', 'Svelte', 'Three.js', 'Postgres'],
-  },
-  {
-    year: '2020 - 2021',
-    role: 'Software Engineer',
-    company: 'Paypal',
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ['Ruby', 'Rails', 'PHP', 'Sqlite'],
+    year: 'August 2024 - Present',
+    role: 'Freelance Frontend Developer',
+    company: 'Church Vision Apostolica - Lomas de Tafí',
+    description: `Designed and developed a responsive one-page website for a local church to showcase their beliefs, events, and image gallery.`,
+    technologies: [
+      'React',
+      'Ant Design',
+      'Figma',
+      'Vercel',
+      'Google Cloud Services',
+    ],
   },
 ];
 
